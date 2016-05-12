60cb7f4026aaf910afa56f422cc4308e640ce221
RogueAI42 <declanhoare0@gmail.com>
2016-05-12 19:52:23 +1000

    tweaking licensing

--- meracomatic.sh
+++ meracomatic.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Merac-O-Matic
-# This program is free software: you can redistribute it and/or modify
+# This file is part of Merac-O-Matic.
+#
+# Merac-O-Matic is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
+# 
+# Merac-O-Matic is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with Merac-O-Matic.  If not, see <http://www.gnu.org/licenses/>.
 intro=$(( ( RANDOM % 13 ) + 1 ))
 body=$(( ( RANDOM % 28 ) + 1 ))
 outro=$(( ( RANDOM % 9 ) + 1 ))
